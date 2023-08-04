160a51a0857f2c577ea5ee51922b0cc24ef3eeec
Tor4ubas <alex1029@yandex.ru>
2023-08-04 21:29:03 +0300

    add new cod 13.1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,18 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+def test_calculate_total_price():
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+    assert item1.calculate_total_price() == 200000
+    assert item2.calculate_total_price() == 100000
+
+def test_apply_discount() -> None:
+
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+
+    item1.apply_discount()
+    item2.apply_discount()
+
+    assert item1.price == 10000
+    assert item2.price == 20000

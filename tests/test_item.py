be5a2a5092adeec5ba66bc7f96e652fbb99eef9c
Tor4ubas <alex1029@yandex.ru>
2023-08-27 13:36:29 +0300

    homework-6

--- tests/test_item.py
+++ tests/test_item.py
@@ -17,6 +17,7 @@ def phone1():
 
     return Phone("IPhone", 30000, 10, 2)
 
+
 def test_1(item1):
 
     """ Тест метода init """
@@ -25,61 +26,40 @@ def test_1(item1):
     assert item1.price == 10000
     assert item1.quantity == 20
 
+
 def test_calculate_total_price(item1):
-    item1 = Item("Смартфон", 10000, 20)
-    item2 = Item("Ноутбук", 20000, 5)
+
+    """ Тест функции calculate_total_price """
+
     assert item1.calculate_total_price() == 200000
-    assert item2.calculate_total_price() == 100000
-
-def test_apply_discount(item1) -> None:
-
-    item1 = Item("Смартфон", 10000, 20)
-    item2 = Item("Ноутбук", 20000, 5)
-
-    item1.apply_discount()
-    item2.apply_discount()
-
-    assert item1.price == 10000
-    assert item2.price == 20000
-
-def test_string_to_number():
-    #assert len(Item.all) == 5  # в файле 5 записей с данными по товарам
-
-    item1 = Item.all[0]
-    assert item1.name == 'Смартфон'
-
-    assert Item.string_to_number('5') == 5
-    assert Item.string_to_number('5.0') == 5
-    assert Item.string_to_number('5.5') == 5
-
-def test_name(item1):
-    item = Item('Смартфон', 10000, 5)
-
-    # длина наименования товара меньше 10 символов
-    item.name = 'Смартфон'
-    assert item.name == 'Смартфон'
 
 
-    Item.instantiate_from_csv()  # создание объектов из данных файла
-    assert len(Item.all) == 5  # в файле 5 записей с данными по товарам
+def test_apply_discount(item1):
 
-    item1 = Item.all[0]
-    assert item1.name == 'Смартфон'
+    """ Тест функции apply_discount """
 
-def test_repr(item1):
+    Item.pay_rate = 0.8
 
-    assert repr(item1) == "Item('Смартфон', 10000, 20)"
+    assert item1.apply_discount() == 8000.0
 
 
-def test_str(item1):
+def test_getter_name(item1):
 
-    assert str(item1) == "Смартфон"
+    """ Тест property-getter """
 
-def test_add(item1, phone1):
+    assert item1.name == "Смартфон"
 
-    """ Тест метода add """
 
-    assert item1 + phone1 == 30
+def test_setter_name(item1):
+
+    """ Тест property-setter """
+
+    item1.name = "IPhone 13 Pro"
+    assert item1.name == "IPhone 13 P"
+
+    item1.name = "IPhone 13"
+    assert item1.name == "IPhone 13"
+
 
 def test_instantiate_from_csv():
 
@@ -88,3 +68,33 @@ def test_instantiate_from_csv():
     Item.all = []
     Item.instantiate_from_csv()
     assert len(Item.all) == 0
+
+
+def test_string_to_number():
+
+    """ Тест статистического метода string_to_number """
+
+    assert Item.string_to_number("5.0") == 5
+    assert Item.string_to_number("5.0") == 5
+    assert Item.string_to_number("5.5") == 5
+
+
+def test_repr(item1):
+
+    """ Тест метода repr """
+
+    assert repr(item1) == "Item('Смартфон', 10000, 20)"
+
+
+def test_str(item1):
+
+    """ Тест метода str """
+
+    assert str(item1) == "Смартфон"
+
+
+def test_add(item1, phone1):
+
+    """ Тест метода add """
+
+    assert item1 + phone1 == 30
